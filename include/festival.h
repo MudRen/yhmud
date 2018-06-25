63184a85e6db166819c065b86950dc1227f7986e
klajie <perjer@sina.com>
2018-06-25 18:13:25 +0800

    6-25

---
+++ include/festival.h
@@ -0,0 +1,13 @@
+// festival.h
+#ifndef __FETEDAY__
+#define __FDETDAY__
+
+//#define BEGIN_TIME		1430409600
+#define FETEMONTH		5
+#define BEGIN_DAY		13 
+#define END_DAY			13 
+#define FETEDAY			"Ä¸Ç×½Ú"
+#define FESTIVAL		"festival/2018/Ä¸Ç×½Ú"
+
+#endif
+
