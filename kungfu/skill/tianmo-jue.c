a2af9ff9277ae67ed924e31dde59fb394924968d
Xuefeng <i@chenxuefeng.cn>
2019-06-10 13:01:55 +0100

    修正错别字等问题

--- kungfu/skill/tianmo-jue.c
+++ kungfu/skill/tianmo-jue.c
@@ -4,19 +4,19 @@ inherit SKILL;
 
 #include <ansi.h>
 
-void create() { seteuid(getuid()); }
+void create()
+{
+    seteuid(getuid());
+}
 
 string type() { return "knowledge"; }
 
 int practice_skill(object me)
 {
-        return notify_fail("天魔诀只能靠学(learn)来提高。\n");
+    return notify_fail("天魔诀只能靠学(learn)来提高。\n");
 }
 void skill_improved(object me)
 {
-    tell_object(me, HIR "你充满了仇恨，想道：“杀！杀！杀！我要杀尽天下人！”\n" NOR );
+    tell_object(me, HIR "你感觉自己内心充满了杀戮的欲望！\n" NOR);
     me->add("shen", -200);
 }
-
-
-
